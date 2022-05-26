196a529bc735a842cc39246414c812c32c0a1dd2
Kim Duclos <kimduclos@gmail.com>
2022-05-26 15:45:09 -0700

    avail form values working

--- src/components/UpdateAvail.jsx
+++ src/components/UpdateAvail.jsx
@@ -9,7 +9,15 @@ const UpdateAvail = () => (
   <div>
     <h1>My Availability</h1>
     <Formik
-      initialValues={{ email: "", color: "red", firstName: "", lastName: "" }}
+      initialValues={{
+        sunday: "not available",
+        monday: "not available",
+        tuesday: "not available",
+        wednesday: "not available",
+        thursday: "not available",
+        friday: "not available",
+        saturday: "not available",
+      }}
       onSubmit={(values, actions) => {
         setTimeout(() => {
           alert(JSON.stringify(values, null, 2));
@@ -19,53 +27,52 @@ const UpdateAvail = () => (
     >
       <Form>
         <h4>Sunday</h4>
-        <Field as="select" name="color">
-          <option value="red">Select Shift</option>
-          <option value="red">Morning: 5am - 2pm</option>
-          <option value="green">Evening: 4pm - 9pm</option>
+        <Field as="select" name="sunday">
+          <option value="">Select Shift</option>
+          <option value="Morning: 5am - 2pm">Morning: 5am - 2pm</option>
+          <option value="Evening: 4pm - 9pm">Evening: 4pm - 9pm</option>
         </Field>
         <h4>Monday</h4>
-        <Field as="select" name="color">
-          <option value="red">Select Shift</option>
-          <option value="red">Morning: 5am - 2pm</option>
-          <option value="green">Evening: 4pm - 9pm</option>
+        <Field as="select" name="monday">
+          <option value="">Select Shift</option>
+          <option value="Morning: 5am - 2pm">Morning: 5am - 2pm</option>
+          <option value="Evening: 4pm - 9pm">Evening: 4pm - 9pm</option>
         </Field>
 
         <h4>Tuesday</h4>
-        <Field as="select" name="color">
-          <option value="red">Select Shift</option>
-          <option value="red">Morning: 5am - 2pm</option>
-          <option value="green">Evening: 4pm - 9pm</option>
+        <Field as="select" name="tuesday">
+          <option value="">Select Shift</option>
+          <option value="Morning: 5am - 2pm">Morning: 5am - 2pm</option>
+          <option value="Evening: 4pm - 9pm">Evening: 4pm - 9pm</option>
         </Field>
 
         <h4>Wednesday</h4>
-        <Field as="select" name="color">
-          <option value="red">Select Shift</option>
-          <option value="red">Morning: 5am - 2pm</option>
-          <option value="green">Evening: 4pm - 9pm</option>
+        <Field as="select" name="wednesday">
+          <option value="">Select Shift</option>
+          <option value="Morning: 5am - 2pm">Morning: 5am - 2pm</option>
+          <option value="Evening: 4pm - 9pm">Evening: 4pm - 9pm</option>
         </Field>
 
         <h4>Thursday</h4>
-        <Field as="select" name="color">
-          <option value="red">Select Shift</option>
-          <option value="red">Morning: 5am - 2pm</option>
-          <option value="green">Evening: 4pm - 9pm</option>
+        <Field as="select" name="thursday">
+          <option value="">Select Shift</option>
+          <option value="Morning: 5am - 2pm">Morning: 5am - 2pm</option>
+          <option value="Evening: 4pm - 9pm">Evening: 4pm - 9pm</option>
         </Field>
 
         <h4>Friday</h4>
-        <Field as="select" name="color">
-          <option value="red">Select Shift</option>
-          <option value="red">Morning: 5am - 2pm</option>
-          <option value="green">Evening: 4pm - 9pm</option>
+        <Field as="select" name="friday">
+          <option value="select">Select Shift</option>
+          <option value="Morning: 5am - 2pm">Morning: 5am - 2pm</option>
+          <option value="Evening: 4pm - 9pm">Evening: 4pm - 9pm</option>
         </Field>
 
         <h4>Saturday</h4>
-        <Field as="select" name="color">
-          <option value="red">Select Shift</option>
-          <option value="red">Morning: 5am - 2pm</option>
-          <option value="green">Evening: 4pm - 9pm</option>
+        <Field as="select" name="saturday">
+          <option value="select">Select Shift</option>
+          <option value="Morning: 5am - 2pm">Morning: 5am - 2pm</option>
+          <option value="Evening: 4pm - 9pm">Evening: 4pm - 9pm</option>
         </Field>
-
 
         <button type="submit">Submit</button>
       </Form>
